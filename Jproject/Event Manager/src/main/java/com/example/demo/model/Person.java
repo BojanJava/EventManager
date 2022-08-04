5097abaee97d7556e3a4f0826141543f84d8c2bd
Bojan <bbojanbuky@yahoo.com>
2022-08-04 13:36:26 +0200

    b2Commit#1 (+persons)

--- Jproject/Event Manager/src/main/java/com/example/demo/model/Person.java
+++ Jproject/Event Manager/src/main/java/com/example/demo/model/Person.java
@@ -1,12 +1,12 @@
 package com.example.demo.model;
 
 import javax.persistence.Entity;
+
+
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-
 import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -14,13 +14,24 @@ import lombok.NoArgsConstructor;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-@Builder
 public class Person {
-
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private int id;
 	private String firstName;
 	private String lastName;
 	private String email;
-}
+	
+	public Person(Integer id, String firstName, String lastName, String email) {
+		this.id = id;
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.email = email;
+	}
+	    
+}		 
+
+
+
+
+
