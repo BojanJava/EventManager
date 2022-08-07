db7ae069f672609c40868d227b26b8304aa0d201
BojanJava <110395881+BojanJava@users.noreply.github.com>
2022-08-07 09:57:19 +0200

    Revert "Branch2"

--- Jproject/Event Manager/src/main/java/com/example/demo/repository/PersonRepository.java
+++ Jproject/Event Manager/src/main/java/com/example/demo/repository/PersonRepository.java
@@ -5,6 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.example.demo.model.Person;
 
 public interface PersonRepository extends JpaRepository<Person, Integer> {
-	
-	
+
 }
